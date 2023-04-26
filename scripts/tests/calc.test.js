958aafdd4d75c7f0542e7ba020683ddd3ceddb2d
vivienrauch <vivienmirjam@gmail.com>
2023-04-26 07:49:38 +0000

    walkthrough

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
+        });
+    });
+    describe("Subtraction function", () => {
+
+    });
+    describe("Multiplication function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+})
